d257ec41450a6cd08faf9ed9847f18542c88d217
Deepti New <dpandey@deeptis-macbook-pro.local>
2018-08-10 13:45:43 -0700

    Initial Commit

---
+++ Branch-Demo-AppTests/Branch_Demo_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Branch_Demo_AppTests.swift
+//  Branch-Demo-AppTests
+//
+//  Created by Deepti Pandey on 8/10/18.
+//  Copyright © 2018 Deepti Pandey. All rights reserved.
+//
+
+import XCTest
+@testable import Branch_Demo_App
+
+class Branch_Demo_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
